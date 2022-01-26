e7e7f13cd957eadb4a3b70cb1400f19dd617b37a
NicksyRop <nicksonkipkorir25@gmail.com>
2022-01-26 22:28:28 +0300

    installed json-server and created json.db file

--- src/App.js
+++ src/App.js
@@ -1,88 +1,86 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-import React , {useState} from 'react'
+const App = () => {
+  const [newName, setNewName] = useState("");
 
+  const [newNumber, setNumber] = useState("");
 
+  const [persons, setPersons] = useState([]);
 
-const App = () =>{
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((res) => {
+      setPersons(res.data);
+    });
+  }, []);
+  const handleChange = (event) => {
+    setNewName(event.target.value);
+  };
 
+  const handleChangeNumber = (event) => {
+    setNumber(event.target.value);
+  };
 
-const [ newName, setNewName] = useState('')
+  const addPerson = (event) => {
+    event.preventDefault();
 
-const [ newNumber, setNumber] = useState('')
+    const obj = {
+      name: newName,
+      number: newNumber,
+    };
 
-const [ persons , setPersons] = useState([
-  {name : "Nickson Kipkorir" , number : 422444242}
-])
+    if (persons.some((person) => person.name === obj.name)) {
+      alert(obj.name + " Already exist in phonebook");
 
-const handleChange = (event) =>{
+      setNewName("");
+    } else {
+      setPersons(persons.concat(obj));
 
-  setNewName(event.target.value);
-}
+      setNewName("");
+      setNumber("");
+    }
+  };
+  const numbers = persons.map((person) => (
+    <ul key={person.name}>
+      {person.name} {person.number}{" "}
+    </ul>
+  ));
+  return (
+    <div>
+      <h1 style={{ color: "purple", border: "2px solid purple" }}>Phonebook</h1>
+      <form onSubmit={addPerson}>
+        <div>
+          <label>Name</label> <br />
+          <input type="text" value={newName} onChange={handleChange}></input>
+        </div>
+        <div>
+          <label>Number</label> <br />
+          <input
+            type="number"
+            value={newNumber}
+            onChange={handleChangeNumber}
+          ></input>
+        </div>
 
-const handleChangeNumber = (event)=>{
+        <div>
+          <button
+            style={{
+              color: "purple",
+              borderRadius: 5,
+              fontSize: 20,
+              border: "2px solid purple",
+            }}
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
 
-  setNumber(event.target.value)
-
-}
-
-const addPerson = (event) =>{
-  event.preventDefault()
-
-  const obj = {
-    name : newName,
-    number : newNumber
-  }
-
-if(persons.some( person => person.name === obj.name)){
-
- alert( obj.name + " Already exist in phonebook")
-
- setNewName('')
-}else{
-
- 
-
-  setPersons(persons.concat(obj))
- 
-  setNewName('')
-  setNumber("")
-}
-
-
-}
-const numbers = persons.map(person => <ul key={person.name}>{person.name} {person.number} </ul>)
-return(
-
-  <div>
-
-    <h1 style={{ color : "purple" , border : "2px solid purple"}}>Phonebook</h1>
-    <form onSubmit={addPerson}>
-      <div>
-        <label>Name</label> <br/>
-        <input type="text" value ={newName} onChange={handleChange}></input>
-      </div>
-      <div>
-        <label>Number</label> <br/>
-        <input type="number" value ={newNumber} onChange={handleChangeNumber}></input>
-      </div>
-
-      <div>
-        <button style={{ color: "purple" , borderRadius : 5 , fontSize: 20 , border: "2px solid purple"}} type="submit">Add</button>
-      </div>
-
-
-    </form>
-    <h2>Numbers</h2>
-
-    {numbers}
-
-
-
-
-
-  </div>
-)
-
-}
+      {numbers}
+    </div>
+  );
+};
 
 export default App;
